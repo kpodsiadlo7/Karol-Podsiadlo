1d2d7c03f9add22446d38555477be49df03c72ad
Karol Podsiadlo <karolekk131@gmail.com>
2022-05-17 20:13:19 +0200

    Usuwanie liczb nieparzystych

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,18 +1,17 @@
 package com.kodilla.testing.collection;
-
 import java.util.*;
 
 public class OddNumbersExterminator {
 
     public List<Integer> exterminate(List<Integer> numbers) {
-        List<Integer> oddNumbersList = new ArrayList<>();
+        List<Integer> evenNumbersList = new ArrayList<>();
 
         for(int num: numbers) {
             if(num %2==0){
-                oddNumbersList.add(num);
+                evenNumbersList.add(num);
             }
         }
-        return oddNumbersList;
+        return evenNumbersList;
     }
 
 }
