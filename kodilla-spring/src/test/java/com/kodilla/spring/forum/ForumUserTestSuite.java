de34bd93f0022679cb9cb1e2aee06d1f8088186a
Karol Podsiadlo <karolekk131@gmail.com>
2022-07-21 22:20:23 +0200

    14.1 bean ForumUser(fix springbootapplication)

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -2,15 +2,14 @@ package com.kodilla.spring.forum;
 
 import org.junit.jupiter.api.Test;
 
-import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ForumUserTestSuite {
 
     @Test
